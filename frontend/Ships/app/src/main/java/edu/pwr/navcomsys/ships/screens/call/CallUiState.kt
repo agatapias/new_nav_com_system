e75eb55c7b736add66c8bb4a6e2b232902e204fa
agata <agata.piasecka@outlook.com>
2024-06-17 21:45:38 +0200

    remove mocks

--- frontend/Ships/app/src/main/java/edu/pwr/navcomsys/ships/screens/call/CallUiState.kt
+++ frontend/Ships/app/src/main/java/edu/pwr/navcomsys/ships/screens/call/CallUiState.kt
@@ -2,8 +2,8 @@ package edu.pwr.navcomsys.ships.screens.call
 
 data class CallUiState(
     val callStatus: CallStatus = CallStatus.Incoming,
-    val caller: String = "Steven Jobs",
-    val receiver: String = "",
+    val caller: String = "andrew",
+    val receiver: String = "statek1",
     val callTime: String = "00:01",
     val isRecording: Boolean = false,
 )
